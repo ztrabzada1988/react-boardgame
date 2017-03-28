a2b3a3594e75ae562af3acb3a75f8a182b031d59
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-03-27 17:07:46 -0700

    server installation

---
+++ client/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default App = () => {
+  return (
+    <h1>Hello from React</h1>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
 
-let app = express();
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
+const app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(8080, () => console.log('Running on localhost:8080'));
